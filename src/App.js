88996fa9f1c11a3e09df803fdce41917007e981b
Javed Akhtar <javed09523@gmail.com>
2023-04-19 07:03:30 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import VendorSignIn from './component/vsignin';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+// import ProposalsData from './components/VenderProposals/ProposalsData';
+// import ProposalsForm from './components/VenderProposals/ProposalsForm';
+// import ProductDetails from './components/EventDetails';
+// import Products from './components/Events';
+// import ProductsInCart from './components/SelectedEvents';
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
+// import UpdateProposal from './components/VenderProposals/UpdateProposal.jsx';
 function App() {
+  // const [items,setItems]=useState([]);
+
+  // useEffect(()=>{
+    
+  //   async function getAllProducts(){
+  //     axios.get("https://event-proposal-page-spg5.onrender.com/proposalsData").then((res)=>{
+  //       setItems(res.data.data)
+  //       console.log(res.data.data);
+  //     })
+  //   }
+  //   getAllProducts()
+  // },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<VendorSignIn/>}/>
+          {/* <Route path="/proposalsData" element={<ProposalsData/>}/>
+          <Route path="/proposalsForm" element={<ProposalsForm/>}/>
+          <Route path="/home" element={<Products items={items}/>}/>
+          <Route path="/product/:id" element={<ProductDetails/>}/>
+          <Route path="/updateproposal/:id" element={<UpdateProposal/>}/>
+          <Route path="/product/cart" element={<ProductsInCart/>}/> */}
+      </Routes>
+    </BrowserRouter>
+  </div>
   );
 }
 
